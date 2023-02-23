08401705f25b2dac10ae3840a4b186bbbf41131c
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-02-23 14:49:34 +0100

    make non destructive aggressive url encode even less destructive, now prevents double-encoding of url encoded characters

--- techdemos/endecoder/encoders/normal/aggressiveUrl.js
+++ techdemos/endecoder/encoders/normal/aggressiveUrl.js
@@ -11,6 +11,7 @@ const ignoreChars = ["/", ".", ":", "?", "&", "+", "="]
 
 function aggressiveUrlEncodeNonDestructive(str) {
     let hold = ""
+    let skip = 0
     if (str.indexOf("://") !== -1) {
         const location = str.indexOf("://")
         hold = str.substring(0, location)
@@ -19,8 +20,15 @@ function aggressiveUrlEncodeNonDestructive(str) {
 
     let result = ''
     for (let i = 0; i < str.length; i++) {
-        if (ignoreChars.includes(str.charAt(i))) {
-            result += str.charAt(i)
+        const charHere = str.charAt(i)
+        if (charHere === "%") skip = 2
+        if (skip >= 0) {
+            result += charHere
+            --skip
+            continue
+        }
+        if (ignoreChars.includes(charHere)) {
+            result += charHere
             continue
         }
         let hex = str.charCodeAt(i).toString(16)
