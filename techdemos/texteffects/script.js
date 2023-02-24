a28106ef2f7a46cf5d8cab0ead68009216760bef
Mylo <36931363+gitmylo@users.noreply.github.com>
2023-02-24 15:52:23 +0100

    Add support for settings

--- techdemos/texteffects/script.js
+++ techdemos/texteffects/script.js
@@ -4,40 +4,73 @@ import {wordcount} from "./effects/wordcount.js"
 
 const inputBox = document.getElementById("input")
 const outputBox = document.getElementById("output")
+const modeCatSelector = document.getElementById("effectModeCat")
 const modeSelector = document.getElementById("effectMode")
 const applyButton = document.getElementById("applyButton")
+const settingsButton = document.getElementById("settingsButton")
 
 const effects = {
-    "random case": {
-        desc: "random upper/lower case",
-        apply: randomCase
-    },
-    "word count": {
-        desc: "Count words, lines and characters",
-        apply: wordcount
+    "general": {
+        "random case": {
+            desc: "Random upper/lower case",
+            apply: randomCase
+        },
+        "word count": {
+            desc: "Count words, lines and characters",
+            apply: wordcount
+        }
     }
 }
 
-// Load the modes into the modeSelector
-for (const effect in effects) {
-    let el = document.createElement("option")
-    el.value = effect
-    el.innerText = effect
-    el.title = effects[effect].desc
-    modeSelector.appendChild(el)
+function loadCategories() {
+    modeCatSelector.innerHTML = ""
+    // Load the categories into the modeSelector
+    for (const cat in effects) {
+        let el = document.createElement("option")
+        el.value = cat
+        el.innerText = cat
+        modeCatSelector.appendChild(el)
+    }
+    loadModes()
+}
+
+function loadModes() {
+    modeSelector.innerHTML = ""
+    // Load the modes into the modeSelector
+    for (const effect in getCurrentCategory()) {
+        let el = document.createElement("option")
+        el.value = effect
+        el.innerText = effect
+        el.title = effect.desc
+        modeSelector.appendChild(el)
+    }
+    changeMode()
+}
+
+function loadSettings() {
+    if (getCurrentmode().settings !== undefined) {
+        getCurrentmode().settings.loadSettings(document.getElementById("settingsTitle"), document.getElementById("settings"))
+    }
 }
 
 function getCurrentmode() {
-    return effects[modeSelector.value]
+    return getCurrentCategory()[modeSelector.value]
+}
+
+function getCurrentCategory() {
+    return effects[modeCatSelector.value]
 }
 
 function changeMode() {
+    loadSettings()
     applyButton.disabled = getCurrentmode().apply === undefined
+    settingsButton.disabled = getCurrentmode().settings === undefined
 }
 
 applyButton.addEventListener("click", () => {
     outputBox.value = getCurrentmode().apply(inputBox.value)
 })
 
+modeCatSelector.addEventListener("change", () => loadModes())
 modeSelector.addEventListener("change", () => changeMode())
-changeMode()
+loadCategories()
---
+++ techdemos/texteffects/settingslib.js
@@ -0,0 +1,91 @@
+class Setting {
+    /**
+     * @param name {string}
+     * @param value {string|boolean|number}
+     * @param min {number}
+     * @param max {number}
+     * @param step {number}
+     */
+    constructor(name = "", value = "", min = 0, max = Number.MAX_VALUE, step = 1) {
+        this.name = name
+        this.value = value
+        this.min = min
+        this.max = max
+        this.step = step
+        if (value === true || value === false) {
+            this.settingType = 0
+        }
+        else if (typeof value === "number") {
+            this.settingType = 2
+        }
+        else {
+            this.settingType = 1
+        }
+    }
+
+    /**
+     * @returns {HTMLLIElement}
+     */
+    getAsElement() {
+        switch (this.settingType) {
+            case 0:
+                const cbel = document.createElement("li")
+                const checkbox = document.createElement("input")
+                checkbox.type = "checkbox"
+                checkbox.value = this.value
+                checkbox.addEventListener("change", () => this.value = checkbox.value)
+                cbel.innerText = `${this.name}: `
+                cbel.appendChild(checkbox)
+                return cbel
+            case 1:
+                const strel = document.createElement("li")
+                const strdiv = document.createElement("div")
+                strdiv.innerText = `${this.name}:`
+                strel.appendChild(strdiv)
+                const strinput = document.createElement("textarea")
+                strinput.value = this.value
+                strinput.addEventListener("change", () => this.value = strinput.value)
+                strel.appendChild(strinput)
+                return strel
+            case 2:
+                const numel = document.createElement("li")
+                const numdiv = document.createElement("div")
+                numdiv.innerText = `${this.name}:`
+                numel.appendChild(numdiv)
+                const numinput = document.createElement("input")
+                numinput.type = "number"
+                numinput.value = this.value.toString()
+                numinput.step = this.step.toString()
+                numinput.min = this.min.toString()
+                numinput.max = this.max.toString()
+                numinput.addEventListener("change", () => this.value = numinput.value)
+                numel.appendChild(numinput)
+                return numel
+        }
+    }
+}
+
+class Settings {
+    /**
+     * @param modName {string}
+     * @param settings {Setting[]}
+     */
+    constructor(modName = "", settings = []) {
+        this.modName = modName
+        this.settings = settings
+    }
+
+    /**
+     * @param titleEl {HTMLElement}
+     * @param parentEl {HTMLElement}
+     */
+    loadSettings(titleEl, parentEl) {
+        titleEl.innerText = this.modName
+        parentEl.innerHTML = ""
+        for (const setting of this.settings) {
+            parentEl.appendChild(setting.getAsElement())
+        }
+    }
+}
+
+export {Setting, Settings}
